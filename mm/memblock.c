81412a06d3ff3ac4f8adda404437963d8f79cd17
timofejaleksandrovic953-cyber <timofejaleksandrovic953@gmail.com>
2026-09-21 12:02:00 +0400

    Update memblock.c

--- mm/memblock.c
+++ mm/memblock.c
@@ -1950,7 +1950,7 @@ static int memsize_kernel_show(struct seq_file *m, void *private)
 	kernel = rmem_reg->size - initmem;
 	text = _etext - _text;
 	rodata = __end_rodata - __start_rodata;
-	if (__start_rodata < _etext)
+	if ((unsigned long)__start_rodata < (unsigned long)_etext)
 		text -= rodata;
 	data = _edata - _sdata;
 	bss = __bss_stop - __bss_start;
